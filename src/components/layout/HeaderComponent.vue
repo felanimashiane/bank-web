ef9e7aab48a5208f7cc0d8681718a065d2e8d273
felanimashiane <73493204+felanimashiane@users.noreply.github.com>
2025-08-05 23:38:27 +0200

    Aaa (#2)
    * testing
    
    * Remove node_modules from repo
    
    * Remove node_modules from repo
    
    * Remove node_modules from repo
    
    * [Felani] added new structure and more components
    
    * [Felani] added new structure and more components
    
    * Delete .idea/.gitignore
    
    * Delete .idea/bank-web.iml
    
    ---------
    
    Co-authored-by: Felani Mashiane <felanimashiane@Admins-MacBook-Pro-3.local>

---
+++ src/components/layout/HeaderComponent.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <header class="header">
+    <nav class="nav">
+      <h1 class="logo">My Banking APP</h1>
+      <ul class="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/casa">CASA</a></li>
+        <li><a href="/business">Business</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.header {
+  background: linear-gradient(135deg, #f6f5f6, #95052a);
+  color: white;
+  padding: 1rem 2rem;
+}
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.logo {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #95052a;
+}
+
+.nav-links {
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+}
+
+.nav-links a {
+  color: white;
+  text-decoration: none;
+}
+
+.nav-links a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ src/modules/businessBanking/views/BusinessLayout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+import BasicDetailsBusiness from "./BasicDetailsBusiness.vue";
+</script>
+
+<template>
+  <div class="view-container">
+    <div class="stepper-container">
+      Stepper container
+    </div>
+    <div class="body-container">
+     <basic-details-business/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.body-container{
+  width: 75%;
+}
+.stepper-container{
+  width: 20%;
+  float: left;
+}
+.view-container{
+  width: 1024px;
+  display: flex;
+  gap: 2rem;
+  margin-top: 4rem;
+}
+</style>
